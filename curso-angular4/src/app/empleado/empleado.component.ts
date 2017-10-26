423337771e50be910a7b7d3089150d6028edb378
mmiron <mmiron@alutiiq.com>
2017-10-26 16:10:53 -0500

    C20. Crear clases, modelos de datos y objetos, se crea una clase en un archivo por separado y se importa.

--- curso-angular4/src/app/empleado/empleado.component.ts
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 
+import {Empleado} from './empleado';
 
 @Component({
 	selector: 'empleado-tag',
@@ -9,5 +10,22 @@ import {Component} from '@angular/core';
 
 
 export class EmpleadoComponent {
-  public title = 'Titulo del componente Empleado.';
+  public title = 'Componente Empleados.';
+  public empleado:Empleado;
+  public trabajadores:Array<Empleado>;
+
+  constructor(){
+    //se puede crear un objeto empleado.
+    this.empleado = new Empleado('George Sfag',34, 'chef',  true);
+    this.trabajadores = [
+      new Empleado('Mamel Sfag',34, 'Gerente',  true),
+      new Empleado('Arianda',33, 'jefa de piso',  true),
+      new Empleado('Anny Gomz',61, 'Atencion a clientes',  true)
+    ];
+  }
+
+  ngOnInit() {
+
+    console.log(this.empleado);
+  }
 }
---
+++ curso-angular4/src/app/empleado/empleado.ts
@@ -0,0 +1,8 @@
+export class Empleado {
+  //Equivale a la declaracion e inicializacion.
+  constructor(public nombre:string,
+    public edad:number,
+    public cargo:string,
+    public contratado:boolean){}
+
+}
