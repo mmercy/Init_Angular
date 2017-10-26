f2ef2a36da3ead0562c9897220823c77fa38d131
mmiron <mmiron@alutiiq.com>
2017-10-26 12:53:45 -0500

    Propiedades y tipos de datos de typescript

--- curso-angular4/src/app/fruta/fruta.component.ts
+++ curso-angular4/src/app/fruta/fruta.component.ts
@@ -1,14 +1,23 @@
 import {Component} from '@angular/core';
 
+/* Decoradores permite indicar propiedades al componente*/
 @Component({
 	selector:'fruta',
 	templateUrl:'./fruta.component.html'
-	/*template: `
-	<h2>{{nombre_componente}} </h2>
-	<p> {{listado_fruta}} </p>`*/
-
 })
+
 export class FrutaComponent{
 	public nombre_componente = 'Componente Fruta';
 	public listado_fruta = 'Naranja, Manzana, Pera y Sandia';
+
+  //Se saltara la visibilidad.
+  private hola = 'Soy privada';
+
+  public nombre:string = 'Jan Perez';
+  public edad:number = 66;
+  public mayorDeEdad:boolean = true;
+  public trabajos:Array<string> = ['Carpintero', 'Albañil', 'Fontanero'];
+
+  comodin:any = 'Cualquier cosa';
+
 }
