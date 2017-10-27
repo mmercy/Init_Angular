3c5be869ba135ead0957c5899491322971f8fdb5
mmiron <mmiron@alutiiq.com>
2017-10-27 10:57:14 -0500

    Directiva ngIf y evento click, ngIf es una directiva para la condicion IF.

--- curso-angular4/src/app/empleado/empleado.component.ts
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 
-import {Empleado} from './empleado';
+import { Empleado } from './empleado';
 
 @Component({
 	selector: 'empleado-tag',
@@ -13,19 +13,24 @@ export class EmpleadoComponent {
   public title = 'Componente Empleados.';
   public empleado:Empleado;
   public trabajadores:Array<Empleado>;
+  public trabajador_externo:boolean;
 
   constructor(){
     //se puede crear un objeto empleado.
-    this.empleado = new Empleado('George Sfag',34, 'chef',  true);
+    this.empleado = new Empleado('George Sfag',34, 'chef',  false);
     this.trabajadores = [
       new Empleado('Mamel Sfag',34, 'Gerente',  true),
       new Empleado('Arianda',33, 'jefa de piso',  true),
       new Empleado('Anny Gomz',61, 'Atencion a clientes',  true)
     ];
+    this.trabajador_externo = true;
   }
 
   ngOnInit() {
-
     console.log(this.empleado);
+  }
+
+  cambiarExterno(valor){
+    this.trabajador_externo = valor;
   }
 }
