af7e2e5c71a2519f6b208daaa342d831851767ad
mmiron <mmiron@alutiiq.com>
2017-11-07 12:31:35 -0600

    C37. Ejercicio de utilizacion de servicios

--- curso-angular4/src/app/home/home.component.ts
+++ curso-angular4/src/app/home/home.component.ts
@@ -11,13 +11,25 @@ import { Component } from '@angular/core';
 export class HomeComponent{
 
   public titulo:string = 'Pagina principal';
+  public listado_ropa : Array<string>;
+  public prenda_a_guardar: string;
 
   constructor(
     private _ropaService: RopaService
   ){}
 
   ngOnInit(){
-    console.log('Tipo de Ropa: ' + this._ropaService.prueba('camiseta'));
+    this.listado_ropa = this._ropaService.getRopa();
+    console.log('Tipo de Ropa: ' + this.listado_ropa);
+  }
+
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prenda_a_guardar);
+    this.prenda_a_guardar = null;
+  }
+  eliminarPrenda(index:number){
+    console.log('Index: ' + index);
+    this._ropaService.deleteRopa(index);
   }
 
 }
--- curso-angular4/src/app/services/ropa.service.ts
+++ curso-angular4/src/app/services/ropa.service.ts
@@ -6,12 +6,26 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class RopaService{
   public nombre_prenda = 'pantalones Vaqueros';
+  public coleccion_ropa = ['pantalon blanco', 'blusa rosa'];
 
   prueba(nombre_prenda){
     console.log('Ejecutando metodo prueba');
     return nombre_prenda;
   }
 
+  addRopa(nombre_prenda:string):Array<string>{
+    this.coleccion_ropa.push(nombre_prenda);
+    return this.getRopa();
+  }
+
+  getRopa():Array<string>{
+    return this.coleccion_ropa;
+  }
+
+  deleteRopa(index:number):Array<string>{
+    this.coleccion_ropa.splice(index, 1);
+    return this.getRopa();
+  }
 }
 
 
