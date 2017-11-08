fe6b4ff159a1d51c37ec68af4668e334b2bcce33
mmiron <mmiron@alutiiq.com>
2017-11-08 13:30:45 -0600

    C45. Ejercicio con formulario, aplicacion de estilos a formulario

--- curso-angular4/src/app/coches/coches.component.ts
+++ curso-angular4/src/app/coches/coches.component.ts
@@ -10,12 +10,19 @@ import { Coche } from './coche';
 
 export class CochesComponent{
   public coche: Coche;
+  public coches: Array<Coche>;
 
   constructor(){
     this.coche = new Coche("","", "");
+    this.coches = [
+      new Coche("Audi A5","120", "Gris"),
+      new Coche("Renault","125", "Cafe Metalico")
+    ];
   }
 
   onSubmit(){
+    this.coches.push(this.coche);
+    this.coche = new Coche("", "", "");
     console.log(this.coche);
   }
 
