fb96c92d30fec283bc3d96ac262970ea3eff71c4
mmiron <mmiron@alutiiq.com>
2017-11-08 16:36:03 -0600

    C48. Peticiones AJAX en Angular (HTTP, Llamadas a servicios REST)

--- curso-angular4/src/app/coches/coches.component.ts
+++ curso-angular4/src/app/coches/coches.component.ts
@@ -13,6 +13,7 @@ import { Coche } from './coche';
 export class CochesComponent{
   public coche: Coche;
   public coches: Array<Coche>;
+  public articulos;
 
 
   constructor(private _peticionesService: PeticionesService){
@@ -25,6 +26,21 @@ export class CochesComponent{
 
   ngOnInit(){
     let result = this._peticionesService.getPrueba();
+    this._peticionesService.getArticulos().subscribe(
+      result =>{
+        this.articulos = result;
+        if(!this.articulos){
+          console.log("Error en el servidor");
+        }
+        console.log(result);
+      },
+      error=>{
+        var errorMessage = <any> error;
+        console.log(errorMessage);
+      }
+
+    );
+
     console.log("Recibio: " + result);
   }
 
--- curso-angular4/src/app/services/peticiones.service.ts
+++ curso-angular4/src/app/services/peticiones.service.ts
@@ -6,9 +6,22 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class PeticionesService {
 
+  public url: string;
+
+  constructor(private _http:Http){
+    this.url = 'https://jsonplaceholder.typicode.com/posts';
+  }
+
   getPrueba(){
-    let retorno:string = 'Hola Mundo .-.';
+    let retorno: string = 'Hola Mundo .-.';
     console.log(retorno);
     return retorno;
+  }
+
+  /**
+   * Hace una peticion por GET a la url indicada.
+   */
+  getArticulos() {
+    return this._http.get(this.url).map(res => res.json());
   }
 }
